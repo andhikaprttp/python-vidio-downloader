7f54870aa6e8ec8dca5ddf34205b8281a42e9d1f
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-21 14:58:11 +0700

    update main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from pytube import YouTube
+from colorama import Fore
+# Fungsi untuk mendownload video YouTube
+def download_video(url):
+    try:
+        yt = YouTube(url)
+        video = yt.streams.get_highest_resolution()  # Menggunakan resolusi tertinggi
+        video.download()  # Mendownload video
+        print("Video berhasil didownload!")
+    except Exception as e:
+        print("Terjadi kesalahan saat mendownload video:", str(e))
+
+print(Fore.GREEN + "=======================")
+print(Fore.RED + "Hati hati penggunaan paket data")
+print(Fore.GREEN + "=======================")
+video_url = input("Masukan Link Vidio : ")
+download_video(video_url)
